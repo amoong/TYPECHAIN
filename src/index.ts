19fb15b8fcd6252647055b3ace8a3a39fb79f4f1
amoong <moo1323@naver.com>
2021-01-20 10:21:46 +0900

    Creating a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-const sayHi = (name:string, age:number, gender:string): void => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}!`);
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-sayHi("Mu", 122, "male");
+const genesisBlock:Block = new Block(0, "32424323423", "", "Hello", 123456);
 
-export {};
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain)
